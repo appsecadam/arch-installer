f3219c9e7de2bc7708ecbd03a9fed3c5d561f3fc
appsecadam <appsecadam@gmail.com>
2020-04-21 21:23:10 -0700

    needed separate script to deal with chroot

--- arch_base.sh
+++ arch_base.sh
@@ -3,8 +3,6 @@
 # Arch Linux Installer by @canihax
 
 ping -c1 google.com >/dev/null 2>&1 || { echo "error no internet connection"; exit; }
-hostname="tetsujin"
-timezone="America/Los_Angeles"
 disk="/dev/sda"
 efi="/dev/sda1"
 swap="/dev/sda2"
@@ -23,29 +21,9 @@ mount "$root" /mnt
 mkdir -p /mnt/boot
 mount "$efi" /mnt/boot
 curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&ip_version=4&use_mirror_status=on" \
- sed s/#Server/Server/g > /etc/pacman.d/mirrorlist 
+| sed s/#Server/Server/g > /etc/pacman.d/mirrorlist 
 pacman -Sy --noconfirm archlinux-keyring
 pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab 
 arch-chroot /mnt 
-echo "root:password" | chpasswd
-ln -sf "/usr/share/zoneinfo/$timezone" /etc/localtime
-hwclock --systohc
-
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-echo "en_US ISO-8859-1" >> /etc/locale.gen
-locale-gen
-
-echo "$hostname" > /etc/hostname
-printf "127.0.1.1\t%s.localdomain\t%s" "$hostname" "$hostname" >> /etc/hosts
-
-pacman --noconfirm --needed -S networkmanager
-systemctl enable NetworkManager
-systemctl start NetworkManager
-
-partuuid=$(lsblk -o path,partuuid | grep "$root" | awk '{print $2}')
-efibootmgr --disk "$disk" --part 1 --create --label "Arch Linux" --loader /vmlinuz-linux --unicode \
-    "root=PARTUUID=$partuuid rw initrd=\intel-ucode.img initrd=\initramfs-linux.img" --verbose
-exit
 reboot
---
+++ chroot.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "root:password" | chpasswd
+ln -sf "/usr/share/zoneinfo/America/Los_Angeles" /etc/localtime
+hwclock --systohc
+
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US ISO-8859-1" >> /etc/locale.gen
+locale-gen
+
+hostname="tetsujin"
+echo "$hostname" > /etc/hostname
+printf "127.0.1.1\t%s.localdomain\t%s" "$hostname" "$hostname" >> /etc/hosts
+
+pacman --noconfirm --needed -S networkmanager
+systemctl enable NetworkManager
+systemctl start NetworkManager
+
+partuuid=$(lsblk -o path,partuuid | grep "/dev/sda3" | awk '{print $2}')
+efibootmgr --disk "/dev/sda1" --part 1 --create --label "Arch Linux" --loader /vmlinuz-linux --unicode \
+    "root=PARTUUID=$partuuid rw initrd=\intel-ucode.img initrd=\initramfs-linux.img" --verbose
+exit
