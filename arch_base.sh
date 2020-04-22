a0765647a1a76b5161db7554719d32f62325983c
appsecadam <appsecadam@gmail.com>
2020-04-21 19:35:32 -0700

    hardcoded for testing

--- arch_base.sh
+++ arch_base.sh
@@ -2,55 +2,19 @@
 
 # Arch Linux Installer by @canihax
 
-calculate_swap() {
-    total_memory=$(awk 'NR==1 {print int($2 / 1000000)-1}' /proc/meminfo)
-    [ "$total_memory" -le 2 ] && echo 1
-    [ "$total_memory" -gt 2 ] && [ "$total_memory" -le 6 ] && echo 2
-    [ "$total_memory" -gt 6 ] && [ "$total_memory" -le 12 ] && echo 3
-    [ "$total_memory" -gt 12 ] && [ "$total_memory" -le 16 ] && echo 4
-    [ "$total_memory" -gt 16 ] && [ "$total_memory" -le 24 ] && echo 5
-    [ "$total_memory" -gt 24 ] && [ "$total_memory" -le 32 ] && echo 6
-    [ "$total_memory" -gt 32 ] && [ "$total_memory" -le 64 ] && echo 8
-    [ "$total_memory" -gt 64 ] && [ "$total_memory" -le 128 ] && echo 11
-}
-
 ping -c1 google.com >/dev/null 2>&1 || { echo "error no internet connection"; exit; }
 hostname="tetsujin"
-timezone=$(cat <<EOF | tzselect
-2
-49
-21
-1
-EOF
-)
-timedatectl set-ntp true
-swap_space="$(calculate_swap)G"
-disk=$(lsblk -o path,type | grep disk | awk '{print $1}')
-cat <<EOF | fdisk "$disk"
-g
-n
-1
-
-+260M
-t
-1
-n
-2
-
-+$swap_space
-t
-2
-19
-n
-3
-
-
-w
-EOF
-partprobe
-efi=$(lsblk -o path,type | grep part | awk 'NR==1 {print $1}')
-swap=$(lsblk -o path,type | grep part | awk 'NR==2 {print $1}')
-root=$(lsblk -o path,type | grep part | awk 'NR==3 {print $1}')
+timezone="America/Los_Angeles"
+disk="/dev/sda"
+efi="/dev/sda1"
+swap="/dev/sda2"
+root="/dev/sda3"
+swap_space="3GiB"
+timedatectl set-ntp true > /dev/null
+yes | parted "$disk" mklabel gpt > /dev/null
+parted "$disk" mkpart primary fat32 1MiB 261MiB > /dev/null
+parted "$disk" mkpart primary linux-swap 261MiB "$swap_space" > /dev/null
+parted "$disk" mkpart primary ext4 "$swap_space" 100% > /dev/null
 mkfs.fat -F32 "$efi"
 mkswap "$swap"
 swapon "$swap"
@@ -58,13 +22,13 @@ yes | mkfs.ext4 "$root"
 mount "$root" /mnt
 mkdir -p /mnt/boot
 mount "$efi" /mnt/boot
-
-curl -s "https://www.archlinux.org/mirrorlist/all/https/" | sed s/#Server/Server/g > /etc/pacman.d/mirrorlist 
+curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&ip_version=4&use_mirror_status=on" \
+ sed s/#Server/Server/g > /etc/pacman.d/mirrorlist 
 pacman -Sy --noconfirm archlinux-keyring
 pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
-passwd
+echo "root:password" | chpasswd
 ln -sf "/usr/share/zoneinfo/$timezone" /etc/localtime
 hwclock --systohc
 
